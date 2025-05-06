4499210325c8319ff864e95b462693c80c24863f
LilianRage <lilian.rage@edu.ece.fr>
2025-05-06 14:41:15 +0200

    Restauration du front depuis sauvegarde

---
+++ excel-mcp-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ExcelConnector from './components/ExcelConnector/ExcelConnector';
+
+function App() {
+  return (
+    <div className="App">
+      <ExcelConnector />
+    </div>
+  );
+}
+
+export default App;
